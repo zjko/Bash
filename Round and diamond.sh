7604c045dcc0d71f106fc444e540b9756250caa1
zjko <laokegongzuoshi@hotmail.com>
2017-10-25 12:37:30 +0800

    right!

---
+++ Round and diamond.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+read -p "please a number " r
+
+for (( y=r; y>=-r; y-=2  ))
+do
+        for (( x = -r  ; x<= r ; x++  ))
+        do
+                if (( x*x+y*y-r*r> 1 || (y<-x+r && y<x+r && y>-x-r && y>x-r    ) ))
+                then echo -n "*"
+                else echo -n " "
+                fi
+
+        done
+        echo ""
+
+done
